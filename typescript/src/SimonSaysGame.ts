1f3370ca380f6c7226d0a1ac8e6a20ad020c793e
pablo-maff <pablo.maff.076@gmail.com>
2024-04-04 19:19:46 +0200

    should return a new color when user guess blue

--- typescript/src/SimonSaysGame.ts
+++ typescript/src/SimonSaysGame.ts
@@ -1,19 +1,23 @@
-import {Color, IColorGenerator} from "./ColorGenerator";
+import { Color, IColorGenerator } from "./ColorGenerator";
 
 export class SimonSaysGame {
+  private color: Color | null = null;
+
   constructor(public generator: IColorGenerator) {}
 
   start(): string {
-    return this.generator.generate();
+    this.color = this.generator.generate();
+
+    return this.color;
   }
 
   guess(response: Color[]): typeof GameOver | Color[] {
-    if (response.every(color => color === Color.yellow)) {
-      return [Color.yellow, Color.yellow]
+    if (!!this.color && response.every((color) => color === this.color)) {
+      return [this.color, Color.yellow];
     }
 
-    return GameOver
+    return GameOver;
   }
 }
 
-export const GameOver = 'Game over!';
+export const GameOver = "Game over!";
--- typescript/tests/Example.spec.ts
+++ typescript/tests/Example.spec.ts
@@ -1,5 +1,5 @@
 import { Color } from "../src/ColorGenerator";
-import {GameOver, SimonSaysGame} from "../src/SimonSaysGame";
+import { GameOver, SimonSaysGame } from "../src/SimonSaysGame";
 
 describe("SimonSaysGame", () => {
   it("should return green when the game starts", () => {
@@ -28,13 +28,26 @@ describe("SimonSaysGame", () => {
     expect(game.guess([Color.blue])).toBe(GameOver);
   });
 
-  it("should return a new color when user guess correctly", () => {
+  it("should return a new color when user guess yellow", () => {
     const game = new SimonSaysGame({
       generate: () => Color.yellow,
     });
 
     game.start();
 
-    expect(game.guess([Color.yellow])).toStrictEqual([Color.yellow, Color.yellow]);
+    expect(game.guess([Color.yellow])).toStrictEqual([
+      Color.yellow,
+      Color.yellow,
+    ]);
+  });
+
+  it("should return a new color when user guess blue", () => {
+    const game = new SimonSaysGame({
+      generate: () => Color.blue,
+    });
+
+    game.start();
+
+    expect(game.guess([Color.blue])).toStrictEqual([Color.blue, Color.yellow]);
   });
 });
